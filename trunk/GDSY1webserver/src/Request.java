f7fb32733793f4e1dbb167a6ebdec5a4baac1c8e
j3ll3nl <j3ll3nl@b3374d16-9d39-11de-b798-49b661d4457b>
2009-09-09 13:09:22 +0000

    Webservert

--- trunk/GDSY1webserver/src/Request.java
+++ trunk/GDSY1webserver/src/Request.java
@@ -9,25 +9,27 @@ public class Request extends HashMap<String, String> {
 	private String method=null;
 	private String uri=null;
 	private String version=null;
+    private SocketInputStream socketInputStream;
 
 	public Request(SocketInputStream sis) throws IOException
 	{
 
 
-	/*
-		String requestline =
+        socketInputStream = new SocketInputStream(sis);
+
+		String requestline = socketInputStream.readLine();
 		String[] splitline = requestline.split(" ");
 		method = splitline[0];
 		uri = splitline[1];
 		version = splitline[2];
 		// nu de headers
 		String line = "";
-		while ((line=myInputStream.readLine()) != null)
+		while ((line=socketInputStream.readLine()) != null)
 		{
 			String[] header = line.split(":",2);
 			put(header[0], header[1]);
 		}
-    */
+    
 	}
 
 	public String getMETHOD()
