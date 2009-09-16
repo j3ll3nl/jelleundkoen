fc7896b81dbf31c54c88d4327d03586ccc60cdb5
koenmldr <koenmldr@b3374d16-9d39-11de-b798-49b661d4457b>
2009-09-16 11:58:44 +0000

    Servlet netter gemaakt

--- trunk/GDSY1webserver/src/Servlet.java
+++ trunk/GDSY1webserver/src/Servlet.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class Servlet {
     public String response;
     public String contentbase;
+    public Request request;
 
     public Servlet(String contentbase){
         this.response = "";
@@ -13,36 +14,78 @@ public class Servlet {
     }
 
     public String service(Request r){
+        this.request = r;
+        
+        if (r.getMETHOD().equals("CONNECT"))
+            this.CONNECT();
+        else if (r.getMETHOD().equals("DELETE"))
+            this.DELETE();
+        else if (r.getMETHOD().equals("GET"))
+            this.GET();
+        else if (r.getMETHOD().equals("HEAD"))
+            this.HEAD();
+        else if (r.getMETHOD().equals("OPTIONS"))
+            this.OPTIONS();
+        else if (r.getMETHOD().equals("POST"))
+            this.POST();
+        else if (r.getMETHOD().equals("PUT"))
+            this.PUT();
+        else if (r.getMETHOD().equals("TRACE"))
+            this.TRACE();
 
-        if (!(r.getMETHOD().equals("GET") || r.getMETHOD().equals("HEAD"))) {
+        return this.response;
+    }
 
-            this.response =  "method not implemented";
+    public void CONNECT() {
+        this.response = "method not implemented";
+    }
 
-        } else if (r.getMETHOD().equals("GET")){
+    public void DELETE() {
+        this.response = "method not implemented";
+    }
 
-            String filename = !(r.getRequestURI().equals("/")) ? ""+r.getRequestURI() : "index.html";
-            File file = new File(this.contentbase + filename);
+    public void GET() {
+        String filename = !(this.request.getRequestURI().equals("/")) ? ""+request.getRequestURI() : "index.html";
+        File file = new File(this.contentbase + filename);
 
-            if (file.exists()) {
+        if (file.exists()) {
 
-                try {
-                    FileReader fr = new FileReader(file);
-                    BufferedReader reader = new BufferedReader(fr);
-                    String line;
+            try {
+                FileReader fr = new FileReader(file);
+                BufferedReader reader = new BufferedReader(fr);
+                String line;
 
-                    while ((line = reader.readLine()) != null) {
-                        this.response += line;
-                    }
-
-                    reader.close();
-                    fr.close();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                while ((line = reader.readLine()) != null) {
+                    this.response += line;
                 }
 
-            } else this.response = "404 ERROR FILE NOT FOUND";
-        }
-        return response;
+                reader.close();
+                fr.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else this.response = "404 ERROR FILE NOT FOUND";
+    }
+
+    public void HEAD() {
+        this.response = "method not implemented";
+    }
+
+    public void OPTIONS() {
+        this.response = "method not implemented";
+    }
+
+    public void POST() {
+        this.response = "method not implemented";
+    }
+
+    public void PUT() {
+        this.response = "method not implemented";
+    }
+
+    public void TRACE() {
+        this.response = "method not implemented";
     }
 }
