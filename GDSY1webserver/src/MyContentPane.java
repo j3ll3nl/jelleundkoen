a747cc24cdd9a5533768913a86dfd9d9a8dbb9df
j3ll3nl <j3ll3nl@b3374d16-9d39-11de-b798-49b661d4457b>
2009-09-23 10:21:26 +0000

    Gui bouwen...

--- GDSY1webserver/src/MyContentPane.java
+++ GDSY1webserver/src/MyContentPane.java
@@ -11,12 +11,7 @@ class MyContentPane extends JPanel{
 
     private MyJRootPane rootPane;
     public MyJLayeredPane layeredPane;
-
-    private JPanel MyJPanel;
-    private JLabel hostLable,portLable,contentbaseLable,ErrorLable;
-    public JComboBox hostCombobox;
-    public JTextField portField,contentbaseField;
-    public JButton actionButton;
+    public MyJPanel MyJPanel;
 
     MyContentPane()
     {
@@ -26,53 +21,7 @@ class MyContentPane extends JPanel{
         setLayout(new BorderLayout(1,3));
         setPreferredSize(new Dimension(900, 400));
 
-            MyJPanel = new JPanel();
-
-            MyJPanel.setLayout(new FlowLayout());
-            MyJPanel.setSize(900, 35);
-
-            hostLable = new JLabel();
-            hostLable.setName("hostLable");
-            MyJPanel.add(hostLable);
-            hostLable.setText("Host: ");
-
-            InetAddress[] adress = null;
-        try {
-            adress = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName());
-        } catch (UnknownHostException ex) {}
-
-            ComboBoxModel hostListModel = new DefaultComboBoxModel(adress);
-            hostCombobox = new JComboBox();
-            hostCombobox.setName("host");
-            MyJPanel.add(hostCombobox);
-            hostCombobox.setModel(hostListModel);
-            hostCombobox.setSelectedItem(adress);
-            //hostCombobox.addItemListener(control);
-
-            portLable = new JLabel();
-            portLable.setName("portLable");
-            MyJPanel.add(portLable);
-            portLable.setText("Port: ");
-
-            portField = new JTextField("1337");
-            portField.setName("portField");
-            MyJPanel.add(portField);
-            portField.setColumns(4);
-
-            contentbaseLable = new JLabel();
-            contentbaseLable.setName("contentbaseLable");
-            MyJPanel.add(contentbaseLable);
-            contentbaseLable.setText("ContentBase: ");
-
-            contentbaseField = new JTextField("C:/"); // Nu kan je bij de contentbase Control.contentbase
-            contentbaseField.setName("contentbaseField");
-            MyJPanel.add(contentbaseField);
-            contentbaseField.setColumns(23);
-
-            actionButton = new JButton("Start");
-            actionButton.setBackground(Color.GREEN);
-            actionButton.setName("actionButton");
-            MyJPanel.add(actionButton);
+        MyJPanel = new MyJPanel();
 
         add(MyJPanel);
 
---
+++ GDSY1webserver/src/MyJPanel.java
@@ -0,0 +1,63 @@
+
+import java.awt.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import javax.swing.*;
+
+
+
+public class MyJPanel extends JPanel {
+
+    private JLabel hostLable,portLable,contentbaseLable,ErrorLable;
+    public JComboBox hostCombobox;
+    public JTextField portField,contentbaseField;
+    public JButton actionButton;
+
+    public MyJPanel(){
+            setLayout(new FlowLayout());
+            setSize(900, 35);
+
+            hostLable = new JLabel();
+            hostLable.setName("hostLable");
+            this.add(hostLable);
+            hostLable.setText("Host: ");
+
+            InetAddress[] adress = null;
+        try {
+            adress = InetAddress.getAllByName(InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException ex) {}
+
+            ComboBoxModel hostListModel = new DefaultComboBoxModel(adress);
+            hostCombobox = new JComboBox();
+            hostCombobox.setName("host");
+            this.add(hostCombobox);
+            hostCombobox.setModel(hostListModel);
+            hostCombobox.setSelectedItem(adress);
+            //hostCombobox.addItemListener(control);
+
+            portLable = new JLabel();
+            portLable.setName("portLable");
+            this.add(portLable);
+            portLable.setText("Port: ");
+
+            portField = new JTextField("1337");
+            portField.setName("portField");
+            this.add(portField);
+            portField.setColumns(4);
+
+            contentbaseLable = new JLabel();
+            contentbaseLable.setName("contentbaseLable");
+            this.add(contentbaseLable);
+            contentbaseLable.setText("ContentBase: ");
+
+            contentbaseField = new JTextField("C:/"); // Nu kan je bij de contentbase Control.contentbase
+            contentbaseField.setName("contentbaseField");
+            this.add(contentbaseField);
+            contentbaseField.setColumns(23);
+
+            actionButton = new JButton("Start");
+            actionButton.setBackground(Color.GREEN);
+            actionButton.setName("actionButton");
+            this.add(actionButton);
+    }
+}
